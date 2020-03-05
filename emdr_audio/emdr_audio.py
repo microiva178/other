78be54b4ff5a92b117bc710d2b2afa0fb4794a50
userf <you@example.com>
2020-03-05 04:47:10 +0300

    emdr

---
+++ emdr_audio/emdr_audio.py
@@ -0,0 +1,62 @@
+from tkinter import *
+import pygame, time
+import threading
+ 
+root = Tk()
+root.title("emdr_audio")
+root.geometry("208x50")
+root.resizable(False, False)
+
+###
+
+b2 = Button(text="140", width=5, height=2)
+ 
+def change():
+    while True:
+        pygame.mixer.init()
+        pygame.mixer.music.load('140.wav')
+        pygame.mixer.music.play()
+        time.wait(104)
+ 
+b2.config(command=change)
+ 
+b2.pack()
+
+b2.place(x=1, y=1)
+
+###
+
+b1 = Button(text="280", width=5, height=2)
+ 
+def change():
+    while True:
+        pygame.mixer.init()
+        pygame.mixer.music.load('280.wav')
+        pygame.mixer.music.play()
+        time.wait(50)
+ 
+b1.config(command=change)
+ 
+b1.pack()
+
+b1.place(x=70, y=1)
+
+###
+
+b0 = Button(text="560", width=5, height=2)
+ 
+def change():
+    while True:
+        pygame.mixer.init()
+        pygame.mixer.music.load("560.wav")
+        pygame.mixer.music.play()
+        time.wait(25)
+ 
+b0.config(command=change)
+ 
+b0.pack()
+
+b0.place(x=139, y=1)
+ 
+root.mainloop()
+
